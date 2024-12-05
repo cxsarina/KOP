bafa90746f4d056009f2cbbbf7c7d0effaeec70d
Aryna Shkolna <cxsarina@gmail.com>
2024-12-05 23:52:34 +0200

    Перероблено логіку роутингу

--- lab4/lab4/src/App.js
+++ lab4/lab4/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import NotebooksAndComputers from './pages/NotebooksAndComputers';
-import Computers from './pages/Computers';
-import ProductPage from './pages/ProductPage';
+import Breadcrumbs from './components/Breadcrumbs';
+import DynamicPage from './pages/DynamicPage';
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/notebooks-and-computers" element={<NotebooksAndComputers />} />
-            <Route path="/notebooks-and-computers/computers" element={<Computers />} />
-            <Route path="/notebooks-and-computers/computers/artline-x43v45" element={<ProductPage />} />
-        </Routes>
+        <div>
+            <Breadcrumbs />
+            <Routes>
+                <Route path="/" element={<DynamicPage type="home" />} />
+                <Route path="/:entity" element={<DynamicPage />} />
+            </Routes>
+        </div>
     );
 };
 
 export default App;
+
---
+++ lab4/lab4/src/components/Breadcrumbs.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+const breadcrumbMap = {
+    notebooks: { label: 'Ноутбуки', parent: '/' },
+    computers: { label: 'Комп\'ютери', parent: '/notebooks' },
+    'artline-x43v45': { label: 'ARTLINE X43v45', parent: '/computers' },
+};
+
+const Breadcrumbs = () => {
+    const location = useLocation();
+    const path = location.pathname.split('/').filter(Boolean)[0];
+
+    const crumbs = [];
+    let currentPath = path;
+
+    while (currentPath) {
+        const crumb = breadcrumbMap[currentPath];
+        if (crumb) {
+            crumbs.unshift(
+                <span key={currentPath}>
+          {' > '}
+                    <Link to={crumb.parent}>{crumb.label}</Link>
+        </span>
+            );
+            currentPath = crumb.parent.split('/').filter(Boolean)[0];
+        } else {
+            currentPath = null;
+        }
+    }
+
+    return (
+        <nav>
+            <Link to="/">Головна</Link>
+            {crumbs}
+        </nav>
+    );
+};
+
+export default Breadcrumbs;
